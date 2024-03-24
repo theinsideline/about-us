04877a865c3f9c26be93153e420dd34bd5774473
Volodymyr Lysenko <morgota199@gmail.com>
2024-03-24 18:56:30 +0200

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <TheHero />
+  <TheServingOurCommunity />
 </template>
 
 <script setup lang="ts">
 import TheHero from "@/components/the-hero/the-hero.vue";
+import TheServingOurCommunity from "@/components/the-serving-our-community/the-serving-our-community.vue";
 </script>
 
 <style>
