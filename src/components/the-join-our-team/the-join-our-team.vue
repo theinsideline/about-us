c93c18892d637c613ccbe2865f59fcef020f6b1a
Volodymyr Lysenko <morgota199@gmail.com>
2024-04-01 04:09:15 +0300

    Optimize slider

--- src/components/the-join-our-team/the-join-our-team.vue
+++ src/components/the-join-our-team/the-join-our-team.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-    import { computed, ref } from 'vue'
     import { PUBLIC_DOMAIN } from '@/constants'
-    import { Carousel, Slide } from 'vue3-carousel'
-    import JoinOurTeamCard from '@/components/the-join-our-team/join-our-team-card.vue'
-    import { Heading2 } from '@theinsideline/common'
+
+    import { computed, ref } from 'vue'
     import { useWindowSize } from '@vueuse/core'
+    import { Carousel, Slide } from 'vue3-carousel'
+    import { Heading2 } from '@theinsideline/common'
+
+    import JoinOurTeamCard from '@/components/the-join-our-team/join-our-team-card.vue'
+
+    const { width } = useWindowSize()
+
+    const isShowMobile = computed(() => width.value >= 320 && width.value <= 767)
+    const isShowTablet = computed(() => width.value >= 768 && width.value <= 1200)
+    const isShowDesktop = computed(() => width.value >= 1201)
 
     const cards = ref([
         {
@@ -33,12 +41,6 @@
             buttonLink: '',
         },
     ])
-
-    const { width } = useWindowSize()
-
-    const isShowMobile = computed(() => width.value >= 320 && width.value <= 767)
-    const isShowTablet = computed(() => width.value >= 768 && width.value <= 1200)
-    const isShowDesktop = computed(() => width.value >= 1201)
 </script>
 
 <template>
