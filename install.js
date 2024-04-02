7e6394a4b4bc76f13f8dbc6890a6551ab749ddbe
morgota199 <55238829+morgota199@users.noreply.github.com>
2024-04-02 18:38:52 +0000

    Deploying to production from @ theinsideline/about-us@c14c1101bd89adaad8cd16b00d6cf69919ce5ffb 🚀

--- install.js
+++ install.js
@@ -1,28 +1,28 @@
 //LOAD "ABOUT US" PAGE FROM GIT REPOSITORY
-const createScriptElement = (url) => {
-  const scriptElement = document.createElement("script");
-  scriptElement.defer = true;
-  scriptElement.src = url;
+const AboutUsInstall = {
+    createScriptElement: url => {
+        const scriptElement = document.createElement('script')
+        scriptElement.defer = true
+        scriptElement.src = url
 
-  return scriptElement;
-};
+        return scriptElement
+    },
+    createStyleElement: url => {
+        const styleElement = document.createElement('link')
+        styleElement.rel = 'stylesheet'
+        styleElement.href = url
 
-const createStyleElement = (url) => {
-  const styleElement = document.createElement("link");
-  styleElement.rel = "stylesheet";
-  styleElement.href = url;
+        return styleElement
+    },
+    load: () => {
+        const appElement = createScriptElement('https://theinsideline.github.io/about-us/js/index.js')
+        const chunkElement = createScriptElement('https://theinsideline.github.io/about-us/js/chunk-vendors.js')
+        const styleElement = createStyleElement('https://theinsideline.github.io/about-us/css/index.css')
 
-  return styleElement;
-};
+        document.head.append(appElement)
+        document.head.append(chunkElement)
+        document.head.append(styleElement)
+    },
+}
 
-const load = () => {
-  const appElement = createScriptElement("https://theinsideline.github.io/about-us/js/index.js");
-  const chunkElement = createScriptElement("https://theinsideline.github.io/about-us/js/chunk-vendors.js");
-  const styleElement = createStyleElement("https://theinsideline.github.io/about-us/css/index.css");
-
-  document.head.append(appElement);
-  document.head.append(chunkElement);
-  document.head.append(styleElement);
-};
-
-document.addEventListener("DOMContentLoaded", load);
+document.addEventListener('DOMContentLoaded', AboutUsInstall.load)
